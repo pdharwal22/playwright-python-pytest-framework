92fd64881a06f0c6cc752cc9d481b3cde9ae0e24
Prateek Dharwal <prateekdharwal890@gmail.com>
2026-07-20 20:45:09 +0530

    Add Configuration skeleton

---
+++ config/config.py
@@ -0,0 +1,16 @@
+class ConfigManager:
+
+    def __init__(self):
+        pass
+
+    def load_environment(self):
+        pass
+
+    def load_configuration(self):
+        pass
+
+    def get(self):
+        pass
+
+    def validate(self):
+        pass
