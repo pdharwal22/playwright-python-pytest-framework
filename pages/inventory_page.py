c6d6cccdea7fe76b7253b1e720749120dbf624ab
Prateek Dharwal <prateekdharwal890@gmail.com>
2026-07-24 21:10:40 +0530

    Add framework logging utility

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -1,11 +1,14 @@
 from playwright.sync_api import Page
 from pages.base_page import BasePage
+from utils.logger import get_logger
 
 
 class InventoryPage(BasePage):
     """
     Page Object representing the SauceDemo inventory page.
     """
+
+    logger = get_logger(__name__)
 
     INVENTORY_CONTAINER = "[data-test='inventory-container']"
     SHOPPING_CART = "[data-test='shopping-cart-link']"
@@ -18,7 +21,10 @@ class InventoryPage(BasePage):
         """
         Check whether the inventory page is displayed.
         """
-        return self.is_visible(self.INVENTORY_CONTAINER)
+        # return self.is_visible(self.INVENTORY_CONTAINER)
+        is_loaded = self.is_visible(self.INVENTORY_CONTAINER)
+        self.logger.info("Inventory page loaded: %s", is_loaded)
+        return is_loaded
     
 
     def add_product_to_cart(self, product_name: str) -> None:
@@ -27,9 +33,17 @@ class InventoryPage(BasePage):
         """
         # product = self.page.locator(".inventory_item").filter(has_text=product_name)
         # product.locator("button").click()
+        
+        self.logger.info("Adding product to cart: %s", product_name)
         product_slug = product_name.lower().replace(" ", "-")
         add_to_cart_button = (f"[data-test='add-to-cart-{product_slug}']")
         self.click(add_to_cart_button)
+        self.logger.info("Product added to cart: %s", product_name)
+
+        # self.logger.info("Adding product to cart: %s", product_name)
+        # product = (self.page.locator(".inventory_item").filter(has_text=product_name))
+        # product.locator(".button").click()
+        # self.logger.info("Product added to cart: %s", product_name)
 
     
     def open_cart(self) -> None:
